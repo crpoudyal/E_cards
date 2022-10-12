9b6063381d8d7a78c74b41ff5d88c8a5a08c17f9
crpoudyal <crpoudyal@gmail.com>
2022-10-12 13:52:01 +0545

    feat: dependencies colorpicker

---
+++ lib/widgets/drawer_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DrawerWidget extends StatefulWidget {
+  const DrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  DrawerWidgetState createState() => DrawerWidgetState();
+}
+
+class DrawerWidgetState extends State<DrawerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: DrawerHeader(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: const [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+          ListTile(
+            title: Text('About'),
+          ),
+          ListTile(
+            title: Text('Profile'),
+          ),
+        ],
+      ),
+    ));
+  }
+}
