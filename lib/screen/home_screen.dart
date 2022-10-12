f994512633a3decbb4097f1f14601ada298c987b
crpoudyal <crpoudyal@gmail.com>
2022-10-12 13:53:50 +0545

    feat: container color changer

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:memesansar/widgets/drawer_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,16 +13,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  String text = "";
+  double top = 10;
+  double left = 10;
+  Color color = Colors.orange;
+  File? _image;
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    final tempImage = File(image.path);
+
+    setState(() {
+      _image = tempImage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("meme sansar"),
-        leading: const Icon(Icons.menu),
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.download)),
         ],
       ),
+      drawer: DrawerWidget(),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
@@ -30,14 +50,17 @@ class HomeScreenState extends State<HomeScreen> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.image)),
-                        IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.add)),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.amp_stories)),
+                            onPressed: () {
+                              getImage();
+                            },
+                            icon: const Icon(Icons.image)),
                         IconButton(
                             onPressed: () {},
+                            icon: const Icon(Icons.text_format)),
+                        IconButton(
+                            onPressed: () {
+                              pickColor(context);
+                            },
                             icon: const Icon(Icons.format_color_fill)),
                         IconButton(
                             onPressed: () {},
@@ -47,15 +70,56 @@ class HomeScreenState extends State<HomeScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      width: double.infinity,
-                      height: 300,
-                      decoration: const BoxDecoration(color: Colors.grey),
+                    RepaintBoundary(
+                      child: GestureDetector(
+                        child: Stack(
+                          children: [
+                            _image != null
+                                ? InteractiveViewer(
+                                    child: Image.file(
+                                      _image!,
+                                      height: 300,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                : Container(
+                                    height: 300,
+                                    width: double.infinity,
+                                    decoration:  BoxDecoration(
+                                      color: color,
+                                    ),
+                                  ),
+                            Positioned(
+                              top: top,
+                              left: left,
+                              child: Text(
+                                text,
+                                style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                        onVerticalDragUpdate: (DragUpdateDetails dd) {
+                          setState(() {
+                            top = dd.localPosition.dy;
+                            left = dd.localPosition.dx;
+                          });
+                        },
+                      ),
                     ),
                     const SizedBox(
                       height: 10,
                     ),
                     TextFormField(
+                      onChanged: (data) {
+                        setState(() {
+                          text = data;
+                        });
+                      },
                       decoration: const InputDecoration(
                         hintText: "Type something...",
                       ),
@@ -73,5 +137,31 @@ class HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  void pickColor(BuildContext context) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text("Pick color"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                buildColorPicker(),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("SELECT",style: TextStyle(fontSize: 16),),
+                )
+              ],
+            ),
+          ));
+
+  Widget buildColorPicker() {
+    return ColorPicker(pickerColor: color, onColorChanged: (color){
+      setState(() {
+        this.color = color;
+      });
+    });
   }
 }
